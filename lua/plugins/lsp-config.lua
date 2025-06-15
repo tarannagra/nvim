5c6de7a28a66a2b98293602f88068c0bb5f1efdf
tarannagra <tarannagra@proton.me>
2025-06-15 19:17:57 +0100

    Installed nvim-lsp including lsp-config
    Using nvim-lsp and lsp-config alongside Mason once more.

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,33 @@
+return {
+    {
+        "mason-org/mason.nvim",
+        opts = {
+            --
+        }
+    },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        config = function()
+            require('mason-lspconfig').setup({
+                ensure_installed = { "lua_ls" }
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+            local lspconfig = require('lspconfig')
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
+            lspconfig.pyright.setup({
+                capabilities = capabilities
+            })
+
+            -- maps
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {}) -- for some reason, it duplicates the hovering thing
+            vim.keymap.set({ 'n', 'v' }, '<Leader>ca', vim.lsp.buf.code_action, {})
+        end
+    },
+}
