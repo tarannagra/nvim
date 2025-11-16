d6eb36027bc9f05f43e8412e4056919506ac1614
tarannagra <tarannagra@proton.me>
2025-11-16 14:45:35 +0000

    added(outline): added outline.nvim

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -1,12 +1,27 @@
 return {
-    {
-        "stevearc/oil.nvim",
-        lazy = false,
-        opts = {
-            default_file_explorer = true,
-            columns = {
-                "icon",
-            },
-        },
-    },
+	{
+		"stevearc/oil.nvim",
+		lazy = false,
+		opts = {
+			default_file_explorer = true,
+			columns = {
+				"icon",
+			},
+		},
+	},
+	{
+		"hedyhli/outline.nvim",
+		lazy = true,
+		cmd = { "Outline", "OutlineOpen" },
+		keys = {
+			{ "<leader>o", "<cmd>OutlineOpen<CR>", desc = "Toggle outline" },
+			{ "<leader>of", "<cmd>OutlineFocus<CR>", desc = "Toggle outline" },
+		},
+		opts = {},
+
+		keymaps = {
+			up_and_jump = "<C-p>",
+			down_and_jump = "<C-n>",
+		},
+	},
 }
