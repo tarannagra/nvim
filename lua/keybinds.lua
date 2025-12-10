d0be3e3b90674021e8a2d4e4e4c933ac650793c2
tarannagra <tarannagra@proton.me>
2025-12-10 14:52:30 +0000

    modified(keybinds): clean up keybinds
    Cleaned up the keybinds file and added more which are needed.

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -1,29 +1,33 @@
+-- Taran's Braino Keybinds
 local api = vim.api
+
+-- Information:
+-- <leader> -> key to press before letters following that key
+-- <C-?> -> Control + key. Example: <C-b> -> Ctrl + b
+-- <A-?> -> Alt + key. Example: <A-o> -> Alt + o
 
 -- Leader Key (similar to a super key)
 vim.g.mapleader = " " -- off to space we go!!!
 
+-- Generic mappings
 vim.keymap.set("n", "<Leader>x", ":set wrap!<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<Leader>R", "<CMD>source %<CR>", { noremap = true, silent = true }) -- reload of config
-
-vim.keymap.set("n", "<C-b>", "<CMD>lua require('FTerm').toggle()<CR>")
-vim.keymap.set("t", "<C-b>", "<C-\\><C-n><CMD>lua require('FTerm').toggle()<CR>")
-vim.keymap.set("n", "<Leader>fa", vim.lsp.buf.format, {})
-
-vim.keymap.set("n", "<Leader>os", ":Obsidian search<CR>", { silent = true, desc = "Perform search on Obsidian vault" })
+vim.keymap.set("n", "<Leader>fa", vim.lsp.buf.format, { desc = "Format the buffer." })
 vim.keymap.set("n", "<Leader>hs", ":split<CR>", { silent = true, noremap = true, desc = "Split buffer horizontally." })
 vim.keymap.set("n", "<Leader>vs", ":vsplit<CR>", { silent = true, noremap = true, desc = "Split buffer vericatlly." })
 
+-- Plugin specific mappings
+vim.keymap.set("n", "<C-b>", "<CMD>lua require('FTerm').toggle()<CR>")
+vim.keymap.set("t", "<C-b>", "<C-\\><C-n><CMD>lua require('FTerm').toggle()<CR>")
+
+-- Obsidian related
+vim.keymap.set("n", "<A-o>", ":Obsidian search<CR>", { silent = true, desc = "Perform search on Obsidian vault" })
+
+-- Oil
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory", silent = true })
 
-vim.keymap.set("n", "of", "<CMD>Telescope oldfiles<CR>", { noremap = true, silent = true, desc = "Filter last used files" })
-vim.keymap.set("n", "gd", "<CMD>lua Snacks.picker.lsp_declarations()<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "gi", "<CMD>lua Snacks.picker.diagnostics()<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<Leader>fg", "<CMD>lua Snacks.picker.grep()<CR>", { noremap = true, silent = true })
-vim.keymap.set(
-	"n",
-	"<Leader>ff",
-	"<CMD>lua Snacks.picker.files({hidden = true,})<CR>",
-	{ noremap = true, silent = true }
-)
-vim.keymap.set("n", "<Leader>H", "<CMD>lua Snacks.dashboard()<CR>", { noremap = true, silent = true })
+-- Picker
+vim.keymap.set("n", "gi", ":FzfLua lsp_document_diagnostics<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>fg", ":FzfLua live_grep<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>ff", ":FzfLua files<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Leader>hh", ":FzfLua helptags<CR>", { noremap = true, silent = true })
+-- vim.keymap.set("n", "<Leader>H", "<CMD>lua Snacks.dashboard()<CR>", { noremap = true, silent = true })
